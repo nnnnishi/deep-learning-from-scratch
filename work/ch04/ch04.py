cdc630597cc8a0bce0f025a1327018f67b8bf1c2
Naoki Nishimura <nishimura.n.ab@gmail.com>
2018-11-22 18:24:15 +0900

    ch04

---
+++ work/ch04/ch04.py
@@ -0,0 +1,197 @@
+#%% [markdown]
+# ニューラルネットワークの学習
+
+#%% [markdown]
+# 数値微分
+
+#%%
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h)-f(x-h))/(2*h)
+
+#%%
+def function_1(x):
+    return 0.01*x**2 + 0.1*x
+
+#%%
+print(numerical_diff(function_1,5))
+print(numerical_diff(function_1,10))
+
+#%% [markdown]
+# 偏微分
+
+#%%
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+#%% [markdown]
+def function_tmp1(x0):
+    return x0**2 + 4.0**2.0
+
+numerical_diff(function_tmp1, 3.0)
+
+#%%
+def function_tmp2(x1):
+    return 3.0**2.0 + x1**2
+
+numerical_diff(function_tmp2, 4.0)
+
+#%% [markdown]
+# 勾配
+
+#%%
+import numpy as np
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1-fxh2)/(2*h)
+        x[idx] = tmp_val
+    return grad
+
+#%%
+numerical_gradient(function_2, np.array([3.0,4.0]))
+
+#%%
+numerical_gradient(function_2, np.array([3.0,0.0]))
+
+#%% [markdown]
+# 勾配降下法
+
+#%%
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
+#%%
+gradient_descent(function_2,
+                 np.array([-3.0, 4.0]),
+                 lr = 0.1,
+                 step_num=100)
+
+
+#%% [markdown]
+# ニューラルネットワークに対する勾配
+
+#%%
+import sys, os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+x = np.array([0.6, 0.9])
+t = np.array([0, 0, 1])
+
+net = simpleNet()
+
+f = lambda w: net.loss(x, t)
+dW = numerical_gradient(f, net.W)
+
+print(dW)
+
+#%% [markdown]
+# 2層ニューラルネットワークのクラス
+
+#%%
+from common.functions import *
+from common.gradient import numerical_gradient
+
+
+class TwoLayerNet:
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 重みの初期化
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+    
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        return y
+        
+    # x:入力データ, t:教師データ
+    def loss(self, x, t):
+        y = self.predict(x)
+        
+        return cross_entropy_error(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+        
+    # x:入力データ, t:教師データ
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        
+        return grads
+        
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+        
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+        
+        dz1 = np.dot(dy, W2.T)
+        da1 = sigmoid_grad(a1) * dz1
+        grads['W1'] = np.dot(x.T, da1)
+        grads['b1'] = np.sum(da1, axis=0)
+
+        return grads
